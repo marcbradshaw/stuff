c1723b6c14fe37790b532fc0f4c37099de32fad5
Marc Bradshaw <marc@marcbradshaw.net>
2014-05-28 08:04:02 +0000

    add PS1 to bash rc

--- bin/bash-setup.sh
+++ bin/bash-setup.sh
@@ -17,7 +17,7 @@ function add_to_bashrc {
     else
         # Not quite 100%, but close enough
         # Does not enforce whole line, FIXME
-        FOUNDLINE=`grep -F "$KEY" ~/.bachrc`
+        FOUNDLINE=`grep -F "$KEY" ~/.bashrc`
     fi
 
     if [ "$FOUNDLINE" == "" ]
@@ -83,6 +83,6 @@ pushd ../remote/smartcd
 make install
 popd
 
+add_to_bashrc "PS1" "\"\[\e[1;32m\]\u\[\e[1;31m\]@\h \[\e[1;33m\]\w\[\e[0m\]\$(__git_ps1 \" (%s)\")$ \""
 add_to_bashrc "export GIT_PS1_SHOWDIRTYSTATE=1"
-# add PS1 here
 
