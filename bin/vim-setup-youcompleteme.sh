45b45dd49f0527fc5946974f56f22c14e7d59cbb
Marc Bradshaw <marc@marcbradshaw.net>
2015-10-29 20:11:17 +1100

    YouCompleteMe install depends if necessary

--- bin/vim-setup-youcompleteme.sh
+++ bin/vim-setup-youcompleteme.sh
@@ -11,6 +11,30 @@ cd ../
 
 ## May need to install cmake and python libs
 ## sudo apt-get install build-essential cmake python-dev
+
+APTGET=""
+
+CMAKE=`which cmake`
+if [ $CMAKE == "" ]
+then
+    APTGET="$APTGET cmake"
+fi
+
+if [ ! -d "/usr/share/build-essential" ]
+then
+    APTGET="$APTGET build-essential"
+fi
+
+if [ ! -d "/usr/share/doc/python-dev" ]
+then
+    APTGET="$APTGET python-dev"
+fi
+
+if [ ! $APTGET == "" ]
+then
+    sudo aptitude install $APTGET
+fi
+
 get_bundle_from_github Valloric YouCompleteMe
 pushd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
