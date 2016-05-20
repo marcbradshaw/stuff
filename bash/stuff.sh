845bd2352d21cf8553be97fe2f1e5c54f37d781e
Marc Bradshaw <marc@marcbradshaw.net>
2016-05-20 10:38:21 +1000

    Always have correct ssh agent environment for ssh and git

--- bash/stuff.sh
+++ bash/stuff.sh
@@ -25,3 +25,24 @@ fi
 PS1='\[\e[1;32m\]\u\[\e[1;31m\]@\h \[\e[1;33m\]\w\[\e[1;31m\]$(__git_ps1 "(\[\e[1;32m\]%s\[\e[1;31m\])")\[\e[0m\]$ '
 export GIT_PS1_SHOWDIRTYSTATE=1
 
+function setup_ssh {
+
+    AGENT_FILE="/run/user/$UID/ssh-agent"
+    AGENT_FILE_TEMP="/run/user/$UID/ssh-agent-2"
+    if [ ! -e "$AGENT_FILE" ] ; then
+        echo "Starting SSH Agent"
+        gpg-agent --daemon --enable-ssh-support --write-env-file $AGENT_FILE
+        cat $AGENT_FILE | \
+            sed -r "s|^GPG_AGENT_INFO=|export GPG_AGENT_INFO=|" | \
+            sed -r "s|^SSH_AUTH_SOCK=|export SSH_AUTH_SOCK=|"   | \
+            sed -r "s|^SSH_AGENT_PID=|export SSH_AGENT_PID=|"   > $AGENT_FILE_TEMP
+        cat $AGENT_FILE_TEMP >> $AGENT_FILE
+        rm $AGENT_FILE_TEMP
+        chmod 600 $AGENT_FILE
+    fi
+    . $AGENT_FILE
+}
+
+alias ssh="setup_ssh;ssh"
+alias git="setup_ssh;git"
+
